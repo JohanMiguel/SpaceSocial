633d180cddb1801bc6451bcd653570c01f21a2ba
JohanMiguel <jtojin-2020591@kinal.edu.gt>
2025-02-17 16:34:17 -0600

    add(SpaceSocial):Archivos para la ejecutacion de User

---
+++ src/user/user.controller.js
@@ -0,0 +1,129 @@
+import { hash } from "argon2";
+import User from "./user.model.js"
+import fs from "fs/promises"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+
+// Actualizar Contraseña
+export const updatePassword = async (req, res) => {
+    try{
+        const { uid } = req.params
+        const { newPassword } = req.body
+
+        const user = await User.findById(uid)
+
+        const matchOldAndNewPassword = await verify(user.password, newPassword)
+
+        if(matchOldAndNewPassword){
+            return res.status(400).json({
+                success: false,
+                message: "La nueva contraseña no puede ser igual a la anterior"
+            })
+        }
+
+        const encryptedPassword = await hash(newPassword)
+
+        await User.findByIdAndUpdate(uid, {password: encryptedPassword}, {new: true})
+
+        return res.status(200).json({
+            success: true,
+            message: "Contraseña actualizada",
+        })
+
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al actualizar contraseña",
+            error: err.message
+        })
+    }
+}
+
+// Actualizar Users
+export const updateUser = async (req, res) => {
+    try {
+        const { uid } = req.params;
+        const  data  = req.body;
+
+        const user = await User.findByIdAndUpdate(uid, data, { new: true });
+
+        res.status(200).json({
+            success: true,
+            msg: 'Usuario Actualizado',
+            user,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            msg: 'Error al actualizar usuario',
+            error: err.message
+        });
+    }
+}
+//Listar User
+export const getUsers = async (req, res) => {
+    try{
+        const { limite = 5, desde = 0 } = req.query
+        const query = { status: true }
+
+        const [total, users ] = await Promise.all([
+            User.countDocuments(query),
+            User.find(query)
+                .skip(Number(desde))
+                .limit(Number(limite))
+        ])
+
+        return res.status(200).json({
+            success: true,
+            total,
+            users
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al obtener los usuarios",
+            error: err.message
+        })
+    }
+}
+
+// Actualizar Foto
+export const updateProfilePicture = async (req, res) => {
+    try{
+        const { uid } = req.params
+        let newProfilePicture = req.file ? req.file.filename : null
+
+        if(!newProfilePicture){
+            return res.status(400).json({
+                success: false,
+                message: "No hay archivo en la petición"
+            })
+        }
+
+        const user = await User.findById(uid)
+
+        if(user.profilePicture){
+            const oldProfilePicture = join(__dirname, "../../public/uploads/profile-pictures", user.profilePicture)
+            await fs.unlink(oldProfilePicture)
+        }
+
+        user.profilePicture = newProfilePicture
+        await user.save()
+
+        return res.status(200).json({
+            success: true,
+            message: "Foto actualizada",
+            profilePicture: user.profilePicture,
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al actualizar la foto",
+            error: err.message
+        })
+    }
+}
